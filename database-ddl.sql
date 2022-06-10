d924f2fe261380728172e1d496020542e05bea55
unknown <acevedo.felipe201@gmail.com>
2022-06-09 22:00:55 -0500

    init database

---
+++ database-ddl.sql
@@ -0,0 +1,64 @@
+CREATE TABLE eqipos (
+    id INT NOT NULL,
+    nombre VARCHAR(255),
+    estado BOOLEAN,
+    fechaCreacion DATETIME,
+    fechaActualizacion DATETIME,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE estados (
+    id INT NOT NULL,
+    nombre VARCHAR(255),
+    estado BOOLEAN,
+    fechaCreacion DATETIME,
+    fechaActualizacion DATETIME,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE tipos (
+    id INT NOT NULL,
+    nombre VARCHAR(255),
+    estado BOOLEAN,
+    fechaCreacion DATETIME,
+    fechaActualizacion DATETIME,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE usuarios (
+    id INT NOT NULL,
+    nombre VARCHAR(255),
+    email VARCHAR(255),
+    estado BOOLEAN,
+    fechaCreacion DATETIME,
+    fechaActualizacion DATETIME,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE marcas (
+    id INT NOT NULL,
+    nombre VARCHAR(255),
+    estado BOOLEAN,
+    fechaCreacion DATETIME,
+    fechaActualizacion DATETIME,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE inventario (
+    serial INT NOT NULL,
+    modelo VARCHAR(255),
+    descripcion VARCHAR(255),
+    urlFoto VARCHAR(255),
+    color VARCHAR(255),
+    fechaCompra DATETIME,
+    precio DECIMAL,
+    idUsuarioACargo INT,
+    CONSTRAINT fk_idUsuarioACargo FOREIGN KEY idUsuarioACargo REFERENCES usuarios(id),
+    idMarca INT,
+    CONSTRAINT fk_idMarca FOREIGN KEY idMarca REFERENCES marca(id),
+    idEstado INT,
+    CONSTRAINT fk_idEstado FOREIGN KEY idEstado REFERENCES estado(id),
+    idIipo INT,
+    CONSTRAINT fk_idIipo FOREIGN KEY idIipo REFERENCES tipo(id),
+    PRIMARY KEY (serial)
+);
